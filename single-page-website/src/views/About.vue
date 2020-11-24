382e59b137413f77b58d3a1497b038cc8b0e8fd6
Mehmetcan Budak <mehmetcanbudak@gmail.com>
2020-11-24 15:36:20 +0300

    worked on about us page

--- single-page-website/src/views/About.vue
+++ single-page-website/src/views/About.vue
@@ -1,5 +1,55 @@
 <template>
-  <div class="about">
-    <h1>This is an about page</h1>
-  </div>
+  <div class="container">
+    <div class="has-text-centered">
+      <h1 class="">About Us</h1>
+    </div>
+    <div class="push-down has-text-centered">
+      <div class="columns">
+        <div class="column">
+          <div class="has-text-left">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Corrupti assumenda dolore sunt doloremque beatae aspernatur error molestiae sequi consequuntur iusto? Itaque magnam fugit iusto, rerum ducimus possimus fugiat praesentium sed doloremque, ullam similique eveniet, vel obcaecati! Iusto dolorum ipsum repellendus porro nisi? Eveniet tenetur quas, ipsa corporis non quos voluptates, omnis ex, veniam similique quia! Illum, cupiditate soluta, magnam error ipsum quia maxime numquam culpa dignissimos placeat quas eos magni eius nisi necessitatibus deleniti? Labore dolores aliquid voluptatum, vitae necessitatibus ad doloremque alias autem deserunt, culpa nemo laboriosam provident soluta perspiciatis minus mollitia est reprehenderit tenetur eos quidem impedit dignissimos.</p>
+          </div>
+        </div>
+        <div class="column">
+          <div class="">
+            <img src="../assets/logo.png" alt="image2">
+          </div>  
+        </div>
+        <div class="columns">
+          <div class="column">
+            <div class="box">
+            </div>  
+          </div>
+          <div class="column">
+            <div class="margin">
+              <ul class="has-text-left">
+                <li>Adress:  Somewhere</li>
+                <li>Email:  something@something.com</li>
+                <li>Phone:  012345442344</li>
+              </ul>  
+            </div>  
+          </div>
+        </div>
+      </div>
+  </div>   
+  </div>     
 </template>
+
+<style media="screen">
+  h1 {
+    text-decoration: underline;
+  }
+  p {
+    margin-top: 40%;
+  }
+  img {
+    margin-right: 10%;
+  }
+  .box {
+    height: 20px;
+    weight: 20px;
+  }
+  .margin {
+    margin-left: 20%;
+  }
+</style>
