e826d894ed4c80e6865d11bb4ad9cd889a983512
Mehmetcan Budak <mehmetcanbudak@gmail.com>
2020-11-24 15:16:28 +0300

    changed image on home page

--- single-page-website/src/views/Home.vue
+++ single-page-website/src/views/Home.vue
@@ -1,8 +1,33 @@
 <template>
   <div class="home">
-    <h1> Home </h1>
+    <div class="container">
+      <div  class="columns">
+        <div class="column auto">
+          <div class="text">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Tempore ad, impedit et minima numquam dolorem obcaecati, deleniti nulla fuga ullam aperiam ea nam, sunt officia. Perspiciatis velit repudiandae ut, labore delectus reprehenderit accusantium, placeat, unde eaque ullam esse odio corrupti. Repudiandae veniam in dolor maxime rem optio repellendus maiores sint, quas quis ducimus architecto saepe reiciendis, doloremque sit vitae dolorem nemo dolore autem animi a cum perferendis ullam! Magni molestias earum minima! Molestiae quasi atque magni deleniti deserunt, cupiditate accusamus numquam quibusdam veritatis exercitationem sit fuga itaque nostrum cum voluptatem explicabo corporis, repellat cumque. Recusandae architecto quasi sequi fuga molestiae!</p>
+          </div>  
+          <div class="margin">
+            <button type="button" class="button is-primary" name="button"><router-link to="/"></router-link>Learn More</button>
+          </div>
+        </div>
+        <div class="column is-three-fifths">
+          <div class="">
+            <img src="../assets/music4.png" alt="image1">
+          </div>  
+        </div>  
+        </div>
+    </div>  
   </div>
 </template>
 
 <script>
 </script>
+
+<style scoped>
+  .text {
+    margin-top: 50%;
+  }
+  button {
+    margin-top: 10%;
+  }
+</style>
